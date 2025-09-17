d7cd54a6ea563d3fd67022a6d6f7c9acabd973c3
Yashvi <yashvi0patel11@gmail.com>
2025-09-17 21:34:47 +0530

    addded state management

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,8 @@
-import ReactDOM from 'react-dom/client.jsx'
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import './index.css'
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store.jsx";
-
+import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
